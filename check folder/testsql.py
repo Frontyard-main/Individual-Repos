0abcaac1c443cea417f2a1c60f65bfd69d015f2f
KeerthanaBharathi <keerthana.ds.2018@gmail.com>
2020-07-29 12:07:59 +0530

    sql commands with update and delete

--- check folder/testsql.py
+++ check folder/testsql.py
@@ -1,6 +1,6 @@
 import sqlite3
 #defining connection
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('register.db')
 
 #defining cursor
 c=conn.cursor()
@@ -12,14 +12,31 @@ create table if not exists register(company_name text primary key ,email_id text
 #inserting values to the table
 #c.execute("""insert into register (company_name ,email_id ,ph_no ) values ('ABC company','abc@gmail.com',45678)""")
 #c.execute("""insert into register (company_name ,email_id ,ph_no ) values ('hgjhv company','hghgf@gmail.com',987689)""")
-conn.commit()
+c.execute("""insert into register (company_name ,email_id ,ph_no ) values ('all in one','all@gmail.com',56784)""")
+
 
 c.execute("select * from register")
 
 rows=c.fetchall()
-print(rows)
-#for r in rows:
- #   print(r)
+for r in rows:
+   print(r)
+#update the email_id of all in one company
+c.execute("update register set email_id='allinone@gmail.com' where company_name='all in one';")
+c.execute("select * from register")
+
+rows=c.fetchall()
+for r in rows:
+   print(r)
+#delete the values in ABC company
+c.execute("delete from register where company_name='ABC company'")
+
+c.execute("select * from register")
+
+rows=c.fetchall()
+for r in rows:
+   print(r)
+
+conn.commit()
 #close connection
 conn.close()
 
